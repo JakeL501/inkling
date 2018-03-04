230c926ae96ad748501eeb8079f1f964dee0c65c
Paul J Hollingsworth <30421725+HollingsworthX51@users.noreply.github.com>
2018-03-04 14:50:21 -0500

    Delete database.js

--- config/database.js
+++
@@ -1,7 +0,0 @@
-// config/database.js
-module.exports = {
-
-//use our mongo data info here
-    'url' : 'your-settings-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
-};
