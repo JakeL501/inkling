baa5aef10e63d53fbcbe88ce9353b3c070038115
vag-tsdc <vagtsdc@gmail.com>
2018-03-06 19:26:02 -0500

    front end clean up, while adam fixes our structure

--- inkling_react/src/App.js
+++ inkling_react/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Jumbotron from "./components/Jumbotron";
-import Panel from "./components/Panel";
-import Carousel from "./components/Carousel";
-import Modal from "./components/Modal-lisu";
-import InkBuild from "./components/Ink-build";
-import InkStyles from "./components/Ink-Styles";
-import Collapse from "./components/Collapse";
-import SignIn from "./components/SignIn";
+import Navbar from "./components/Navbar/Navbar";
+import Jumbotron from "./components/Jumbotron/Jumbotron";
+import Panel from "./components/Panel/Panel";
+import Carousel from "./components/Carousel/Carousel";
+import Modal from "./components/Modal-lisu/Modal-lisu";
+import InkBuild from "./components/Ink-build/Ink-build";
+import InkStyles from "./components/Ink-Styles/Ink-Styles";
+import Collapse from "./components/Collapse/Collapse";
+import SignIn from "./components/SignIn/SignIn";
 
 const App = () => (
   <div className="container">
