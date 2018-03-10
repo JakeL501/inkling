1c60075b442fd83dff66b87762ee5b5560ff25dd
vag-tsdc <vagtsdc@gmail.com>
2018-03-09 20:25:01 -0500

    alterations to frontend

---
+++ inkling_react/scripts/aboutBuildInk.js
@@ -0,0 +1,46 @@
+const styleSeed = [
+    {
+      style: "Traditional",
+      img: "http://placehold.it/700x300",
+      des:
+        "The traditional style was born on the high seas during the 1700s, after sailors like those who sailed with Capitan James Cook first encountered indigenous communities with tattoos and, becoming inspired by their bodily markings, decided to start collecting body art as mementos of their long voyages. Over the course of the 19th century, the style became more refined taking on the boldly lined and brightly colored aesthetic seen in the work of well-known American tattooists of the 1900s like Sailor Jerry.",
+      // date: new Date(Date.now())
+    },
+    {
+        style: "Traditional",
+        img: "http://placehold.it/700x300",
+        des:
+          "The traditional style was born on the high seas during the 1700s, after sailors like those who sailed with Capitan James Cook first encountered indigenous communities with tattoos and, becoming inspired by their bodily markings, decided to start collecting body art as mementos of their long voyages. Over the course of the 19th century, the style became more refined taking on the boldly lined and brightly colored aesthetic seen in the work of well-known American tattooists of the 1900s like Sailor Jerry.",
+        // date: new Date(Date.now())
+      },{
+        style: "Traditional",
+        img: "http://placehold.it/700x300",
+        des:
+          "The traditional style was born on the high seas during the 1700s, after sailors like those who sailed with Capitan James Cook first encountered indigenous communities with tattoos and, becoming inspired by their bodily markings, decided to start collecting body art as mementos of their long voyages. Over the course of the 19th century, the style became more refined taking on the boldly lined and brightly colored aesthetic seen in the work of well-known American tattooists of the 1900s like Sailor Jerry.",
+        // date: new Date(Date.now())
+      },{
+        style: "Traditional",
+        img: "http://placehold.it/700x300",
+        des:
+          "The traditional style was born on the high seas during the 1700s, after sailors like those who sailed with Capitan James Cook first encountered indigenous communities with tattoos and, becoming inspired by their bodily markings, decided to start collecting body art as mementos of their long voyages. Over the course of the 19th century, the style became more refined taking on the boldly lined and brightly colored aesthetic seen in the work of well-known American tattooists of the 1900s like Sailor Jerry.",
+        // date: new Date(Date.now())
+      },{
+        style: "Traditional",
+        img: "http://placehold.it/700x300",
+        des:
+          "The traditional style was born on the high seas during the 1700s, after sailors like those who sailed with Capitan James Cook first encountered indigenous communities with tattoos and, becoming inspired by their bodily markings, decided to start collecting body art as mementos of their long voyages. Over the course of the 19th century, the style became more refined taking on the boldly lined and brightly colored aesthetic seen in the work of well-known American tattooists of the 1900s like Sailor Jerry.",
+        // date: new Date(Date.now())
+      },{
+        style: "Traditional",
+        img: "http://placehold.it/700x300",
+        des:
+          "The traditional style was born on the high seas during the 1700s, after sailors like those who sailed with Capitan James Cook first encountered indigenous communities with tattoos and, becoming inspired by their bodily markings, decided to start collecting body art as mementos of their long voyages. Over the course of the 19th century, the style became more refined taking on the boldly lined and brightly colored aesthetic seen in the work of well-known American tattooists of the 1900s like Sailor Jerry.",
+        // date: new Date(Date.now())
+      },{
+        style: "Traditional",
+        img: "http://placehold.it/700x300",
+        des:
+          "The traditional style was born on the high seas during the 1700s, after sailors like those who sailed with Capitan James Cook first encountered indigenous communities with tattoos and, becoming inspired by their bodily markings, decided to start collecting body art as mementos of their long voyages. Over the course of the 19th century, the style became more refined taking on the boldly lined and brightly colored aesthetic seen in the work of well-known American tattooists of the 1900s like Sailor Jerry.",
+        // date: new Date(Date.now())
+      }
+    ]
