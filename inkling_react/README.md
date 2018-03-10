137886d1497948ca334b45f858b48b1be0158986
JakeL501 <jacob.letourneau@my.colby-sawyer.edu>
2018-03-10 13:12:28 -0500

    readme file added

---
+++ inkling_react/README.md
@@ -0,0 +1,36 @@
+Inkling
+With Inkling our goal is to connect a potential client with a tattoo artist. The problems we want to solve are as follows:
+ - limit the amount of poorly chosen tattoo's, 
+ - allow clients to think more about what tattoo they want as well as get feedback from artists,
+ - avoid walk-in appointments with people who don't know what they wanted to do. 
+
+Getting Started:
+First clone the repository locally using "git clone" in the git bash terminal and adding the link https://github.com/JakeL501/inkling.git.
+
+After this cd into inkling then inkling_react and do an npm install. When done cd into client and do the same. After all the npm installs are complete cd out of client back to inkling_react and run yarn start. 
+
+React should automatically open your localhost on your web browser.
+
+Prerequisites:
+git bash
+code/text editor - Visual Studio Code is recommended
+
+Heroku Link:
+https://ink-ling.herokuapp.com/
+
+Built With:
+MERN Stack - MongoDB, Express, React, Node
+CRUD Support - Create, Read, Update, Delete 
+Instagram Developer - Connects us to user instagram profiles
+
+Authors:
+Jacob Letourneau - Instagram API 
+Adam Magee - Server/database work
+Paul Hollingsworth - Passport Authentication
+Victoria Griggs - Frontend Design
+
+Acknowledgments:
+Pat 
+Instagram
+InkHunter
+Tattoodo
--- inkling_react/client/src/pages/Artist.js
+++ inkling_react/client/src/pages/Artist.js
@@ -4,7 +4,7 @@ import Panel from "../components/Panel";
 import Footer from "../components/Footer";
 import Jumbotron from "../components/Jumbotron";
 import Navbar from "../components/Navbar";
-import { Input } from "../../../../../../ClassContent/20-react/01-Activities/10-Stu_AJAXFormDelete/Solved/client/src/components/Form";
+// import { Input } from "client/src/components/Form";
 
 class Artist extends Component {
     state ={
@@ -22,21 +22,68 @@ class Artist extends Component {
         )
         .catch(err => console.log(err));
     };
-     render() {
+    render() {
         return (
-            <Container>
-                <Row>
-                    <Col size="md-6">
-                        <Jumbotron>
-                            <h1></h1>
-                        </Jumbotron>
-                        <form>
-                            <Input
-                            />
-                        </form>
-                    </Col>
-                </Row>
-            </Container>
-        )
+          <Container fluid>
+            <Row>
+              <Col size="md-6">
+                <Jumbotron>
+                  <h1>What Books Should I Read?</h1>
+                </Jumbotron>
+                <form>
+                  <Input
+                    value={this.state.title}
+                    onChange={this.handleInputChange}
+                    name="title"
+                    placeholder="Title (required)"
+                  />
+                  <Input
+                    value={this.state.author}
+                    onChange={this.handleInputChange}
+                    name="author"
+                    placeholder="Author (required)"
+                  />
+                  <TextArea
+                    value={this.state.synopsis}
+                    onChange={this.handleInputChange}
+                    name="synopsis"
+                    placeholder="Synopsis (Optional)"
+                  />
+                  <FormBtn
+                    disabled={!(this.state.author && this.state.title)}
+                    onClick={this.handleFormSubmit}
+                  >
+                    Submit Book
+                  </FormBtn>
+                </form>
+              </Col>
+              <Col size="md-6 sm-12">
+                <Jumbotron>
+                  <h1>Books On My List</h1>
+                </Jumbotron>
+                {this.state.books.length ? (
+                  <List>
+                    {this.state.books.map(book => {
+                      return (
+                        <ListItem key={book._id}>
+                          <a href={"/books/" + book._id}>
+                            <strong>
+                              {book.title} by {book.author}
+                            </strong>
+                          </a>
+                          <DeleteBtn onClick={() => this.deleteBook(book._id)} />
+                        </ListItem>
+                      );
+                    })}
+                  </List>
+                ) : (
+                  <h3>No Results to Display</h3>
+                )}
+              </Col>
+            </Row>
+          </Container>
+        );
+      }
     }
-}
+    
+    export default Books;
